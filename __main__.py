f94f842230b898bfc5068b6815dcb9f9b66c3461
owlen <oren@orengampel.com>
2018-05-01 14:08:28 +0300

    add main for testing and validation

---
+++ __main__.py
@@ -0,0 +1,19 @@
+import sys
+import os.path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# pylint: disable=wrong-import-position
+# Python imports are silly.
+import logger
+
+# pylint: enable=wrong-import-position
+
+logger.setup()
+
+LOGGER = logger.logging.getLogger('pkt.logger')
+
+if __name__ == '__main__':
+    LOGGER.debug("a debug message")
+    LOGGER.info("an info message")
+    LOGGER.warning("a warning message")
+    LOGGER.error("an error message")
