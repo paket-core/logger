3f94e6ec277c43a1768a8ab53f695052f3c50fc8
Yaroslav R <yaroslav.r@it-dimension.com>
2018-05-15 17:13:30 +0300

    Removed sys import from tests

--- tests/logger_test.py
+++ tests/logger_test.py
@@ -1,5 +1,4 @@
 """Tests for logger package."""
-import sys
 from unittest import TestCase
 
 import logger
@@ -8,16 +7,16 @@ import logger
 class TestLogger(TestCase):
     """Testing logger."""
     def test_output(self):
-        """Try all levels."""
+        """Try all levels"""
         logger_name = 'pkt.logger'
         pkt_logger = logger.logging.getLogger(logger_name)
         logger.setup()
         # pylint: disable=protected-access
         levels = logger.logging._levelToName
         # pylint: enable=protected-access
-        for level in sorted(list(levels.keys())[1:]):
+        for level in sorted(levels.keys()):
             level_name = levels[level]
-            message = 'Testing - %s - %s%s' % (level_name, sys.argv[0], sys.argv[1:])
+            message = 'Testing - %s' % level_name
             try:
                 logging_function = getattr(pkt_logger, level_name.lower())
                 logging_function(message)
@@ -25,4 +24,4 @@ class TestLogger(TestCase):
                     logging_function(message)
                     self.assertIn(message, log_capture.output[0])
             except AttributeError:
-                pkt_logger.warning("No logging function for %s(%s)", level_name, level)
+                pkt_logger.warning('No logging function for %s(%s)', level_name, level)
