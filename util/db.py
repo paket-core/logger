267f20d818e7c9ee7ff96be87ff1e7352aa3939d
Israel Levin <israel@paket.global>
2018-05-29 20:08:25 +0300

    Add db util.

---
+++ util/db.py
@@ -0,0 +1,18 @@
+"""Database utils."""
+import contextlib
+import sqlite3
+
+@contextlib.contextmanager
+def sql_connection(db_name):
+    """Context manager for querying the database."""
+    try:
+        connection = sqlite3.connect(db_name)
+        connection.row_factory = sqlite3.Row
+        yield connection.cursor()
+        connection.commit()
+    except sqlite3.Error as db_exception:
+        raise db_exception
+    finally:
+        if 'connection' in locals():
+            # noinspection PyUnboundLocalVariable
+            connection.close()
