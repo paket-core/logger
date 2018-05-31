f93fa1edaa6e6adfbd84dd5702efff1e564692bb
Yaroslav R <yaroslav.r@it-dimension.com>
2018-06-01 00:55:17 +0300

    Added numeric representation as option

--- util/stellar_units.py
+++ util/stellar_units.py
@@ -3,23 +3,28 @@
 DEC = 7
 
 
-def stroops_to_units(amount):
+def stroops_to_units(amount, numeric_representation=False):
     """
     Convert amount presented in stroops to units.
     :param int amount: Amount of stroops to be converted
+    :param numeric_representation: If true, result will be returned as float number, otherwise - string
     """
-    if not isinstance(amount, int):
+    if not isinstance(amount, int) or isinstance(amount, bool):
         raise TypeError
 
     amount = '{amount:0>{decimals}}'.format(amount=amount, decimals=DEC)
     amount = '0.{}'.format(amount) if len(amount) == DEC else '{}.{}'.format(amount[:-DEC], amount[-DEC:])
+
+    if numeric_representation:
+        amount = float(amount)
     return amount
 
 
-def units_to_stroops(amount):
+def units_to_stroops(amount, numeric_representation=False):
     """
     Convert amount presented in units to stroops.
     :param str amount: Amount of units to be converted
+    :param numeric_representation: If true, result will be returned as integer number, otherwise - string
     """
     if not isinstance(amount, str) and not isinstance(amount, float):
         raise TypeError
@@ -32,4 +37,7 @@ def units_to_stroops(amount):
         leading_zeros_amount = len(fractional_part) - len(striped)
         trailing_zeros_amount = DEC - leading_zeros_amount - len(striped)
         amount = '{0}{1}'.format(striped, '0' * trailing_zeros_amount)
+
+    if numeric_representation:
+        amount = int(amount)
     return amount
