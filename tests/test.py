982b8d19f196485207a08fe3ad49f1bdad56ade4
Israel Levin <israel@paket.global>
2018-06-05 16:18:47 +0300

    Add new tests to tests list.

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,7 @@
 """Run all tests."""
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
+from tests.countly_test import *
+from tests.db_test import *
 from tests.logger_test import *
+from tests.stellar_units_test import *
