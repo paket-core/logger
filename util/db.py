4f99b32fc10387d15ca307609712d8c5ca23c5ce
Israel Levin <israel@paket.global>
2018-06-17 02:39:24 +0300

    Add database user and password env vars.

--- util/db.py
+++ util/db.py
@@ -1,14 +1,17 @@
 """Database utils."""
 import contextlib
+import os
 
 import mysql.connector
 
+USER = os.environ['PAKET_DB_USER']
+PASSWORD = os.environ['PAKET_DB_PASSWD']
 
 @contextlib.contextmanager
-def sql_connection(db_name):
+def sql_connection(db_name=None, user=None, password=None):
     """Context manager for querying the database."""
     try:
-        connection = mysql.connector.connect(database=db_name)
+        connection = mysql.connector.connect(database=db_name, user=user or USER, passwd=password or PASSWORD)
         yield connection.cursor(dictionary=True)
         connection.commit()
     except mysql.connector.Error as db_exception:
