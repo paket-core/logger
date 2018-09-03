146cdc71e6109273a2dfbf5e508abafa51e10e90
Yaroslav R <yaroslav.r@it-dimension.com>
2018-09-03 15:07:26 +0300

    Update test.

--- tests/distance_test.py
+++ tests/distance_test.py
@@ -5,9 +5,14 @@ import util.distance
 
 
 class DistanceTest(unittest.TestCase):
-    """"""
+    """Test for distance calculations."""
 
     def test_distance(self):
-        from_location = '52.447240,13.416683'
-        to_location = '49.227132,31852766'
+        from_location = '47.858048,35.104131'
+        to_location = '47.834367,35.147932'
+        expected_distance = 4.19
+        deviation = 0.5
         distance = util.distance.haversine(from_location, to_location)
+        self.assertTrue(
+            expected_distance + deviation > distance > expected_distance - deviation,
+            'expected result: {}; given result: {}'.format(expected_distance, distance))
